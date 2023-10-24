84e85a375da91cd259178ab56d6ba488a89d29a3
swift-man <jiniopening@naver.com>
2023-10-25 01:35:36 +0900

    [Refactor] L to S Converter

--- Sources/KoreanLunarSolarConverter/Public/KoreanLunarToSolarConverter.swift
+++ Sources/KoreanLunarSolarConverter/Public/KoreanLunarToSolarConverter.swift
@@ -7,86 +7,108 @@
 
 import Foundation
 
+/// A class to convert Korean lunar dates to solar dates.
 public final class KoreanLunarToSolarConverter {
   private let dataSource = KoreanLunarDataSource()
   private let lunarAlgorithm = KoreanLunarAlgorithm()
   private let solarAlgorithm = KoreanSolarAlgorithm()
   private let lunarDateRangeChecker = LunarDateRangeChecker()
   
-  public init() {
-    
-  }
-
+  /// Default initializer.
+  public init() {}
+  
+  /// Converts a lunar date to a solar date.
+  /// - Parameter lunarDate: The lunar date to convert.
+  /// - Returns: A `KoreanDate` object containing the converted solar date.
+  /// - Throws: An error if the conversion fails.
   public func solarDate(fromLunar lunarDate: Date) throws -> KoreanDate {
-    guard lunarDateRangeChecker.isValidDate(lunarDate: lunarDate)
-    else { throw KoreanLunarConvertError.invalidDate }
-
-    let isIntercalation = dataSource.lunarIntercalationMonth(year: lunarDate.year) == lunarDate.month
-    
-    let date: Date
-    do {
-      date = try solarDate(fromLunarDate: lunarDate, isIntercalation: isIntercalation)
-    } catch {
-      throw error
+    guard
+      lunarDateRangeChecker.isValidDate(lunarDate: lunarDate)
+    else {
+      throw KoreanLunarConvertError.invalidDate
     }
+    
+    let isIntercalation = dataSource.lunarIntercalationMonth(year: lunarDate.year) == lunarDate.month
+    let date = try solarDate(fromLunarDate: lunarDate, isIntercalation: isIntercalation)
     
     return KoreanDate(date: date, isIntercalation: isIntercalation)
   }
 }
 
 extension KoreanLunarToSolarConverter {
+  /// Returns a solar date based on a given lunar date and intercalation status.
+  /// - Parameters:
+  ///   - date: The lunar date to convert.
+  ///   - isIntercalation: Indicates whether the date is intercalary.
+  /// - Returns: The converted solar date.
+  /// - Throws: An error if the conversion fails.
   private func solarDate(fromLunarDate date: Date, isIntercalation: Bool) throws -> Date {
-    let lunarYear = date.year
-    let absDays = lunarAlgorithm.lunarAbsDays(year: lunarYear,
+    let absDays = lunarAlgorithm.lunarAbsDays(year: date.year,
                                               month: date.month,
                                               day: date.day,
                                               isIntercalation: isIntercalation)
     
-    let solarYear = calculateSolarYear(fromLunarYear: lunarYear, absDays: absDays)
-    var solarMonth = 0
-    var solarDay = 0
-
-    for month in stride(from: 12, through: 1, by: -1) {
-      let absDaysByMonth = solarAlgorithm.solarAbsDays(year: solarYear,
-                                                       month: month,
-                                                       day: 1)
-      if absDays >= absDaysByMonth {
-        solarMonth = month
-        solarDay = absDays - absDaysByMonth + 1
-        break
-      }
-    }
+    let solarYear = calculateSolarYear(fromLunarYear: date.year, absDays: absDays)
+    let (solarMonth, solarDay) = calculateSolarMonthAndDay(solarYear: solarYear, absDays: absDays)
     
     guard
-      !isRemovedGregorianCalendarDate(solarYear: solarYear,
-                                      solarMonth: solarMonth,
-                                      solarDay: solarDay)
-    else { throw KoreanLunarConvertError.invalidDate }
-
+      !isRemovedDateInGregorian(solarYear: solarYear,
+                                solarMonth: solarMonth,
+                                solarDay: solarDay)
+    else {
+      throw KoreanLunarConvertError.removedGregorianDate
+    }
+    
     var solarDate = Date()
     solarDate.year = solarYear
     solarDate.month = solarMonth
     solarDate.day = solarDay
-
+    
     return solarDate
   }
   
+  /// Calculates the solar year based on a given lunar year and absolute days.
+  /// - Parameters:
+  ///   - lunarYear: The lunar year to use for the calculation.
+  ///   - absDays: The absolute days to use for the calculation.
+  /// - Returns: The calculated solar year.
   private func calculateSolarYear(fromLunarYear lunarYear: Int, absDays: Int) -> Int {
     if absDays < solarAlgorithm.solarAbsDays(year: lunarYear + 1,
                                              month: 1,
                                              day: 1) {
       return lunarYear
     }
-    
     return lunarYear + 1
   }
   
-  private func isRemovedGregorianCalendarDate(solarYear: Int,
-                                              solarMonth: Int,
-                                              solarDay: Int) -> Bool {
-    if solarYear == 1582 && solarMonth == 10 {
-      return solarDay > 4 && solarDay < 15
+  /// Calculates the solar month and day based on a given solar year and absolute days.
+  /// - Parameters:
+  ///   - solarYear: The solar year to use for the calculation.
+  ///   - absDays: The absolute days to use for the calculation.
+  /// - Returns: A tuple containing the calculated solar month and day.
+  private func calculateSolarMonthAndDay(solarYear: Int, absDays: Int) -> (Int, Int) {
+    for month in stride(from: 12, through: 1, by: -1) {
+      let absDaysByMonth = solarAlgorithm.solarAbsDays(year: solarYear,
+                                                       month: month,
+                                                       day: 1)
+      if absDays >= absDaysByMonth {
+        return (month, absDays - absDaysByMonth + 1)
+      }
     }
-    return false
+    return (0, 0)
+  }
+  
+  /// Checks if a given solar year, month, and day corresponds to a date removed in the Gregorian calendar.
+  /// - Parameters:
+  ///   - solarYear: The solar year to check.
+  ///   - solarMonth: The solar month to check.
+  ///   - solarDay: The solar day to check.
+  /// - Returns: `true` if the date was removed in the Gregorian calendar, `false` otherwise.
+  private func isRemovedDateInGregorian(solarYear: Int,
+                                        solarMonth: Int,
+                                        solarDay: Int) -> Bool {
+    let gregorianTransitionYear = 1582
+    let gregorianTransitionMonth = 10
+    return solarYear == gregorianTransitionYear && solarMonth == gregorianTransitionMonth && solarDay > 4 && solarDay < 15
   }
 }
