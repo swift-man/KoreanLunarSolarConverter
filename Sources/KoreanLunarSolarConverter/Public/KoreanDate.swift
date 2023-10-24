3b475d4bd57f47a2a28679665a531231cf66e7bd
swift-man <jiniopening@naver.com>
2023-10-25 01:34:58 +0900

    [Refactor] KoreanDate

--- Sources/KoreanLunarSolarConverter/Public/KoreanDate.swift
+++ Sources/KoreanLunarSolarConverter/Public/KoreanDate.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+/// Represents a Korean date which might be an intercalation.
 public struct KoreanDate {
+  /// The actual date.
   public let date: Date
+  /// Indicates if the date is an intercalation.
   public let isIntercalation: Bool
   
+  /// Initializes a new KoreanDate.
+  /// - Parameters:
+  ///   - date: The date to represent.
+  ///   - isIntercalation: Indicates if the date is an intercalation.
   public init(date: Date, isIntercalation: Bool) {
     self.date = date
     self.isIntercalation = isIntercalation
